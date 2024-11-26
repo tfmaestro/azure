78cc11f9a2277439c6a03cd12abf7af0c1fc061c
Katarzyna Brzozowska <kbrzozowska@KatarzynaBs-MacBook-Pro-2.local>
2024-11-26 23:51:27 +0100

    Rebuild dev and prod for vnet

--- tfmaestro/envs/prod/variables.tf
+++ tfmaestro/envs/prod/variables.tf
@@ -18,8 +18,3 @@ variable "environment" {
   description = "Environment for the resources"
   type        = string
 }
-
-# variable "resource_group_name" {
-#   description = "Name of the resource group"
-#   type        = string
-# }
