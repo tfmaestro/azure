04871c364f7bbf35504bfa7cbb05b9ac04076217
Katarzyna Brzozowska <kbrzozowska@KatarzynaBs-MacBook-Pro-2.local>
2024-12-03 23:05:55 +0100

    Adding database vnet and prod configuration

--- tfmaestro/envs/prod/main.tf
+++ tfmaestro/envs/prod/main.tf
@@ -2,19 +2,14 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.name}-rg"
   location = var.location
 }
-module "vnet" {
-  source              = "../../modules/vnet/"
-  name                = var.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = var.address_space
-  resource_group_name = azurerm_resource_group.rg.name
 
-  subnets = {
-    "prod-subnet-01" = {
-      address_prefix = "10.0.1.0/24"
-    }
-    "prod-subnet-02" = {
-      address_prefix = "10.0.2.0/24"
-    }
-  }
+module "vnet" {
+  source                  = "../../modules/vnet/"
+  name                    = var.name
+  location                = azurerm_resource_group.rg.location
+  address_space           = var.address_space
+  resource_group_name     = azurerm_resource_group.rg.name
+  private_subnets         = var.private_subnets
+  public_subnets          = var.public_subnets
+  public_database_subnets = var.public_database_subnets
 }
